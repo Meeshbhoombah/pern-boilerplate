7af90d2f7cc5ccdc546550332fb9786ca266b3f1
Rohan Mishra <rohanmishra2017@gmail.com>
2021-02-10 02:27:26 -0800

    Added Clock with simple example

--- src/App.js
+++ src/App.js
@@ -6,24 +6,48 @@ class Clock extends React.Component {
     constructor(props) {
         super(props);
         this.state = {date: new Date()};
+    };
+
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    };
+
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    };
+
+    
+    tick() {
+        this.setState({
+            date: new Date()
+        });
     }
+
 
     render() {
         return (
           <div>
-            <h1>Hello, world!</h1>
-            <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+            <h1>It is {this.state.date.toLocaleTimeString()}</h1>
+            <h2>{this.props.message}</h2>
           </div>
         )
     }
 }
 
+
 function App() {
-  return (
-    <div>
-        <Clock />
-    </div>
-  );
+    const msg = "Test"
+
+    return (
+        <div>
+            <Clock message={msg}/>
+        </div>
+    );
 };
 
 
